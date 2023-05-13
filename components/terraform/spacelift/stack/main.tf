ea3d58d61213357b5aab2ffca265c3391e1d9bce
Matt Calhoun <matt@mattcalhoun.com>
2023-05-12 20:34:42 -0400

    commit initial repo structure

---
+++ components/terraform/spacelift/stack/main.tf
@@ -0,0 +1,15 @@
+locals {
+  enabled = module.this.enabled
+}
+
+module "spacelift" {
+  source = "git@github.com:cloudposse/terraform-spacelift-cloud-infrastructure-automation.git//modules/spacelift-stack?ref=chore/cleanup-stack-module"
+  #version = "0.55.0"
+  # https://github.com/cloudposse/terraform-spacelift-cloud-infrastructure-automation.git
+  stack_name       = var.stack_name
+  repository       = var.repository
+  space_id         = var.space_id
+  atmos_stack_name = var.atmos_stack_name
+  component_name   = var.component_name
+  component_root   = var.component_root
+}
