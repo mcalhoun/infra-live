3c566d8d4aed8a2c6b43d3ba19da08dd937ac1ee
Matt Calhoun <matt@mattcalhoun.com>
2023-05-15 12:29:17 -0400

    add scripts

--- components/terraform/spacelift/stack/main.tf
+++ components/terraform/spacelift/stack/main.tf
@@ -13,4 +13,15 @@ module "spacelift" {
   component_name   = var.component_name
   component_root   = var.component_root
   manage_state     = true
+
+  before_init = [
+    "/mnt/workspace/source/rootfs/usr/local/bin/spacelift-install-atmos",
+    "/mnt/workspace/source/rootfs/usr/local/bin/spacelift-configure-paths",
+    "/mnt/workspace/source/rootfs/usr/local/bin/spacelift-write-vars",
+    "/mnt/workspace/source/rootfs/usr/local/bin/spacelift-tf-workspace"
+  ]
+
+  before_plan  = ["/mnt/workspace/source/rootfs/usr/local/bin/spacelift-configure-paths"]
+  before_apply = ["/mnt/workspace/source/rootfs/usr/local/bin/spacelift-configure-paths"]
+  
 }
